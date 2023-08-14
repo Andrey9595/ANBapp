826619ddf8124ffc44c75164b21792a78419252c
Andrey9595 <anb95.95@mail.ru>
2023-08-14 20:57:12 +0300

    add tests registration

--- app/src/androidTest/java/ru/anb/myapplication/RegisterTest.kt
+++ app/src/androidTest/java/ru/anb/myapplication/RegisterTest.kt
@@ -1,5 +1,6 @@
 package ru.anb.myapplication
 
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.*
@@ -9,6 +10,7 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import ru.anb.myapplication.core.App
 import ru.anb.myapplication.core.ui.MainActivity
 
 @RunWith(AndroidJUnit4::class)
@@ -19,6 +21,7 @@ class RegisterTest {
 
     @Test
     fun inputTestText() {
+
         onView(withId(R.id.navigate_to_sign_up)).perform(click())
 
         val list = listOf(R.id.register_login, R.id.register_name, R.id.pass_layout_password, R.id.pass_layout_check_password)
@@ -27,5 +30,17 @@ class RegisterTest {
             onView(withId(it)).perform(typeText("check"))
             onView(withId(it)).check(matches(withText("check")))
         }
+    }
+    @Test
+    fun transitionTest(){
+
+        val context = ApplicationProvider.getApplicationContext<App>()
+
+        onView(withId(R.id.navigate_to_sign_up)).perform(click())
+        onView(withId(R.id.pass_layout_check_password)).perform(typeText("123"))
+        onView(withId(R.id.start_sign_up)).perform(click())
+        onView(withId(R.id.sign_up_login)).check(matches(withErrorInInputLayout(context.getString(R.string.login_error))))
+        onView(withId(R.id.error_text)).check(matches(isDisplayed()))
+        onView(withId(R.id.error_text)).check(matches(withText(context.getString(R.string.password_error))))
     }
 }
